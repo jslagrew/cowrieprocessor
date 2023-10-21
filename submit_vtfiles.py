a29e65c9d28c552fcaf9801e4dfea28d0832503c
jslagrew <82918323+jslagrew@users.noreply.github.com>
2023-10-21 07:23:35 -0500

    add sha256 hashing of files
    submissions only worked with cowrie files that were named using sha-256 hash. The updates will hash the file to be used for comments added into VT submissions. This also allows the submission and comments added to files that were not named using the sha-256 hash of the file.

--- submit_vtfiles.py
+++ submit_vtfiles.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import requests
 import time
+import hashlib
 
 parser = argparse.ArgumentParser(description='Virus Total file submission options')
 parser.add_argument('--filepath', dest='filepath', type=str, help='Path of a specific file to submit')
@@ -15,31 +16,40 @@ filepath = args.filepath
 folderpath = args.folderpath
 vtapi = args.vtapi
 
-
-
-def vt_filescan(hash):
+def vt_filescan(filename):
     headers = {'X-Apikey': vtapi}
     url = "https://www.virustotal.com/api/v3/files"
-    with open(folderpath + hash, 'rb') as file:
-        files = {'file': (folderpath + hash, file)}
+    with open(folderpath + filename, 'rb') as file:
+        files = {'file': (folderpath + filename, file)}
         response = requests.post(url, headers=headers, files=files)
     json_response = json.loads(response.text)
     if not os.path.exists("vtsubmissions"):
         os.mkdir("vtsubmissions")
-    file = open("vtsubmissions/files_" + hash, 'w')
+    file = open("vtsubmissions/files_" + filename, 'w')
     file.write(response.text)
     file.close()
 
+    filehash = sha256sum(folderpath + filename)
     headers = {
         'Content-type': 'application/json',
         'X-Apikey': vtapi}
-    url = "https://www.virustotal.com/api/v3/files/" + hash + "/comments"
+    url = "https://www.virustotal.com/api/v3/files/" + filehash + "/comments"
     commentdata = {'data':{'type': 'comment', 'attributes': {'text': 'File submitted from a DShield Honeypot - https://github.com/DShield-ISC/dshield'}}}
     response = requests.post(url, headers=headers, data=json.dumps(commentdata))
     json_response = json.loads(response.text)
-    file = open("vtsubmissions/files_comment_" + hash, 'w')
+    file = open("vtsubmissions/files_comment_" + filename, 'w')
     file.write(response.text)
     file.close()
+
+
+def sha256sum(filename):
+    h  = hashlib.sha256()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        while n := f.readinto(mv):
+            h.update(mv[:n])
+    return h.hexdigest()
 
 past = time.time() - ((60*60)/11) #1/11 of an hour - just under 6 minutes
 result = []
@@ -54,4 +64,3 @@ for each_file in result:
     vt_filescan(each_file)
 
 #vt_filescan("58458d88aeb274ebd87a2cc4dad0b64f3c38c8951a287b3b31c1f99c8240d38e")
-#vt_filescan("e94c45e125a530a6590210423d364fd3850dac5df64c78061a4aa913ba89b372")
